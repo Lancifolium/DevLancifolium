2bbd9c3d69ab8e3cee00bbd2474d53db018ba27a
Fierralin <fierralin@hotmail.com>
2016-04-25 10:23:26 +0800

    edit main

--- DevLancifolium/main.cpp
+++ DevLancifolium/main.cpp
@@ -6,11 +6,15 @@
 
 int main(void) {
 	struct DevLancifolium sig;
+	struct DevLancifolium sigb;
 	//sig.openfile("tmp.sgf"); // file
 	char buff[1000];
 	while (~scanf("%s", buff)) {
 		sig.configManual(buff);
 		sig.printfManual();
+		sig.adjustManual(sig.root);
+		sig.printfManual();
 		sig.clearall();
+		sigb.clearall();
 	}
 }
